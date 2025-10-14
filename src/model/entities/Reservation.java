1a53d34d3ac476f3d75e36ded2b09ba179ee5538
Eliezer Miguel <eliezermiguel97@outlook.com>
2025-10-14 10:46:45 -0300

    feat: Class Resevertion created

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private LocalDate checkIn;
+	private LocalDate checkOut;
+	
+	private static DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	public Reservation() {
+	}
+	
+	public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public LocalDate getCheckIn() {
+		return checkIn;
+	}
+
+	public LocalDate getCheckOut() {
+		return checkOut;
+	}
+
+	public long duration() {
+		long diff = checkIn
+                .atStartOfDay(ZoneId.systemDefault())
+                .toInstant()
+                .toEpochMilli() - 
+                checkOut
+                .atStartOfDay(ZoneId.systemDefault())
+                .toInstant()
+                .toEpochMilli();
+		
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
+	}
+	
+	public void updatesDates(LocalDate checkIn, LocalDate checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation:  Room " + roomNumber + ", check-in: " + checkIn.format(formatador) + ", check-out: " + checkOut.format(formatador) + ", " + duration() + " nights";
+	}
+	
+}
